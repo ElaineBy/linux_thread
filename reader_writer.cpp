2b937818f6d416b9a4e0560d58ee11b5b5b02dbd
胡其成 <huqicheng@huqicheng.local>
2017-11-24 23:53:19 -0500

    commit

--- reader_writer.cpp
+++ reader_writer.cpp
@@ -37,12 +37,12 @@ void* a1(void* args){
     s.i1 = s.initial * 1;
     cout << "a1:" << s.i1 << endl;
     
-    mulock(LOCK,&mut_cnt);
+    
     cnt++;
     if(cnt == 3){
         mulock(UNLOCK,&mut_output);
     }
-    mulock(UNLOCK,&mut_cnt);
+    
     mulock(UNLOCK,&mut_input);
 }
 
@@ -51,12 +51,12 @@ void* a2(void* args){
     mulock(LOCK,&mut_input);
     s.i2 = s.initial * 2;
     cout << "a2:" << s.i2 << endl;
-    mulock(LOCK,&mut_cnt);
+    
     cnt++;
     if(cnt == 3){
         mulock(UNLOCK,&mut_output);
     }
-    mulock(UNLOCK,&mut_cnt);
+    
     mulock(UNLOCK,&mut_input);
 }
 
@@ -65,12 +65,12 @@ void* a3(void* args){
     mulock(LOCK,&mut_input);
     s.i3 = s.initial * 3;
     cout << "a3:" << s.i3 << endl;
-    mulock(LOCK,&mut_cnt);
+    
     cnt++;
     if(cnt == 3){
         mulock(UNLOCK,&mut_output);
     }
-    mulock(UNLOCK,&mut_cnt);
+    
     mulock(UNLOCK,&mut_input);
 }
 
@@ -81,10 +81,10 @@ void* io(void* args){
     
     mulock(LOCK,&mut_output);
     
-    mulock(LOCK,&mut_cnt);
+    
     cout << "set counter 0" << endl;
     cnt = 0;
-    mulock(UNLOCK,&mut_cnt);
+    
     
     mulock(UNLOCK,&mut_output);
     
